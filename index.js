937f8066a782606094d2c7ad4d270c81ecb9924f
Zach Schwartz <zachatorcodes@gmail.com>
2022-10-21 11:25:34 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, searchName) {
+    return (array.filter((name) => (name.toLowerCase() === searchName.toLowerCase())));
+}
+
+function fuzzyMatch(array, searchName) {
+    return (array.filter((name) => (name.toLowerCase().charAt(0) === searchName.toLowerCase().charAt(0))));
+}
+
+function matchName(array, searchName) {
+    return (array.filter((obj) => (obj.name.toLowerCase() === searchName.toLowerCase())));
+}
